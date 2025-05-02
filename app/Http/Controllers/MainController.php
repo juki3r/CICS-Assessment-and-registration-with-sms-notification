154a03001bfdeb64f40e3cd9da9e743ff355f2a3
juki3r <deepong25@gmail.com>
2025-05-02 10:42:02 +0800

    modified:   app/Http/Controllers/MainController.php 	modified:   app/Http/Controllers/MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -16,6 +16,39 @@ use App\Services\SMS;
 class MainController extends Controller
 {
 
+    protected $apiUrl = 'https://sms.pong-mta.tech/api/send-sms-api';
+    protected $apiKey;
+
+    public function __construct()
+    {
+        $this->apiKey = env('PONG_SMS_TOKEN', 'your_default_token_here');
+    }
+
+    public function sendsms($phoneNumber, $message)
+    {
+        if (!$this->apiKey) {
+            return ['success' => false, 'error' => 'API key is not configured'];
+        }
+
+        $response = Http::withHeaders([
+            'X-API-KEY' => $this->apiKey,
+            'Content-Type' => 'application/json',
+        ])->post($this->apiUrl, [
+            'phone_number' => $phoneNumber,
+            'message' => $message,
+        ]);
+
+        if ($response->failed()) {
+            return [
+                'success' => false,
+                'error' => 'Failed to send message',
+                'details' => $response->body(),
+            ];
+        }
+
+        return ['success' => true, 'response' => $response->json()];
+    }
+
     public function dashboard()
     {  
         $notificationCount = AdminNotification::where('read', false)->count();
