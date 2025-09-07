748315975d072523b33e004d4db0b2292421627b
juki3r <deepong25@gmail.com>
2025-09-07 15:28:20 +0800

    modified:   resources/views/layouts/app.blade.php 	modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,26 +8,57 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     <style>
-        .nav-link{
-            font-size: 22px;
+        .nav-link {
+            font-size: 18px !important;
+            color: #000; /* or your preferred default color */
+            padding-bottom: 5px;
         }
+
+        .nav-link.active-nav {
+            border-bottom: 3px solid #ffc107; /* yellow border for active */
+            font-weight: bold;
+            color: #ffc107 !important;
+        }
+
     </style>
 </head>
 <body class="d-flex flex-column vh-100 p-0">
+
+    <div class="d-flex justify-content-center py-3 bg-dark text-light shadow">
+        <div class="pe-4 text-center">
+            <img src="{{asset('logo.png')}}" alt="" width="100px">
+        </div>
+        <div class="">
+            <h6 class="text-center">Republic of the Philippines</h6>
+            <h3 class="fs-3 text-center">
+                NORTHERN ILOILO STATE UNIVERSITY
+            </h3>
+            <h6 class="text-center" style="font-weight: light !important">
+                NISU Main Campus, V Cudilla Sr. Ave, Estancia, Iloilo
+            </h6>
+        </div>
+        <div class="ps-4 text-center">
+           <img src="{{asset('cics.png')}}" alt="" width="100px">
+        </div>
+    </div>
+
+
+
+
     <!-- Navigation -->
     @include('layouts.navigation')
 
     <!-- Page Heading -->
     @isset($header)
-    <header class="">
+    {{-- <header class="">
         <div class="p-2 px-3">
             {{ $header }}
         </div>
-    </header>
+    </header> --}}
     @endisset
 
     <!-- Page Content -->
-    <main class="flex-grow-1 p-2 px-3">
+    <main class="flex-grow-1 p-2 px-3 ">
         {{ $slot }}
     </main>
     
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,45 +1,62 @@
-<div class="row w-100 py-2 shadow bg-dark text-light">
-        {{-- LOGO --}}
-        <div class="col-1 d-flex justify-content-center ">
-           <img src="{{ asset('cics.png') }}" alt="Logo" style="width:100px">
-        </div>
-        {{-- NAV --}}
-        <div class="col d-flex align-items-center">
-           <a href="{{route('admin.dashboard')}}" :active="request()->routeIs('admin.dashboard')" class="nav-link mx-3">
-                Dashboard
-           </a>
-           <a href="{{route('admin.users')}}" class="nav-link mx-3">
-                User accounts
-           </a>
-           <a href="{{route('admin.registrations')}}" class="nav-link mx-3">
-                Registration
-           </a>
-           <a href="{{route('admin.interview')}}" class="nav-link mx-3">
-                Interview
-           </a>
-           <a href="{{route('show.questions')}}" class="nav-link mx-3">
-                Test Questionaires
-           </a>
-           <a href="{{route('show.questions')}}" class="nav-link mx-3">
-                Reports
-           </a>
-           <a href="{{route('show.questions')}}" class="nav-link mx-3">
-                Sms Logs
-           </a>
-           <a href="" class="m-auto me-0 text-warning">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
-            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
-            </svg>
-           </a>
-        </div>
+<div class="row w-100 py-2">
+    {{-- NAV --}}
+    <div class="col d-flex align-items-center">
 
-        {{-- USER --}}
-           <div class="col-1 d-flex justify-content-center align-items-center">
-            <a href=""  class="text-light">
-                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                </svg>
-            </a>
-           </div>
-       </div>
+        <a href="{{ route('admin.dashboard') }}"
+           class="nav-link mx-3 {{ request()->routeIs('admin.dashboard') ? 'active-nav' : '' }}">
+            Dashboard
+        </a>
+
+        <a href="{{ route('admin.users') }}"
+           class="nav-link mx-3 {{ request()->routeIs('admin.users') ? 'active-nav' : '' }}">
+            User accounts
+        </a>
+
+        <a href="{{ route('admin.registrations') }}"
+           class="nav-link mx-3 {{ request()->routeIs('admin.registrations') ? 'active-nav' : '' }}">
+            Registration
+        </a>
+
+        <a href="{{ route('admin.interview') }}"
+           class="nav-link mx-3 {{ request()->routeIs('admin.interview') ? 'active-nav' : '' }}">
+            Interview
+        </a>
+
+        <a href="{{ route('show.questions') }}"
+           class="nav-link mx-3 {{ request()->routeIs('show.questions') ? 'active-nav' : '' }}">
+            Test Questionaires
+        </a>
+
+        <a href="{{ route('show.questions') }}"
+           class="nav-link mx-3 {{ request()->routeIs('show.questions') ? 'active-nav' : '' }}">
+            Reports
+        </a>
+
+        <a href="{{ route('show.questions') }}"
+           class="nav-link mx-3 {{ request()->routeIs('show.questions') ? 'active-nav' : '' }}">
+            Sms Logs
+        </a>
+
+        
+    </div>
+
+    {{-- USER --}}
+    <div class="col-1 d-flex justify-content-evenly  align-items-center">
+          <a href="" class=" text-warning">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                 class="bi bi-bell-fill" viewBox="0 0 16 16">
+                <path
+                    d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+            </svg>
+        </a>
+
+        <a href="" class="text-dark">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                 class="bi bi-person-circle" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                <path fill-rule="evenodd"
+                      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+            </svg>
+        </a>
+    </div>
+</div>
