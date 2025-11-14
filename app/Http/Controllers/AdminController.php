1eee62c8d5f6483f299a5e0c15b2c95350a0904a
juvlyn <juvlynbibas16@gmail.com>
2025-11-14 12:08:02 +0800

    paaa

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -708,7 +708,7 @@ class AdminController extends Controller
             $phone = $student->contact_details;
             $course = $student->course;
 
-            $message = "Congratulations {$name}, you are qualified incoming
+            $message = "{$name}, you are qualified incoming
                         First year student in " . strtoupper($student->course) . " A.Y 2025-2026. Dont reply because this is system generated";
 
             // $response = Http::withHeaders([
